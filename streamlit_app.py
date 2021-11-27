ac503c7fea1bcbd1bcf6b8bc37bd41212917107f
Steven Burman <steven.burman@gmail.com>
2021-11-27 11:56:22 +1000

    Add chart

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,8 @@ import numpy as np
 from os import listdir
 from os.path import isfile, join
 
+import altair as alt
+
 root_dir = './data'
 onlyfiles = [f for f in listdir(root_dir) if isfile(join(root_dir, f))]
 
@@ -35,4 +37,19 @@ if cat_selected is None or cat_selected == "All":
     st.dataframe(data=_, height=800)
 else:
     _ = df[df["category"] == cat_selected].sort_values(['rarity_score'], ascending=False)
-    st.dataframe(data=_, height=800)
+
+    bar = alt.Chart(_).mark_bar().encode(
+        x='trait',
+        y='rarity_score'
+    )
+
+    rule = alt.Chart(_).mark_rule(color='red').encode(
+            y='mean(rarity_score)'
+    )
+
+    chart = (bar + rule).properties(width=600)
+    st.altair_chart(chart, use_container_width=False)
+
+    st.table(data=_)
+
+
