d79c50d0adc7eff5eb58ec3b54683b7887695906
Steven Burman <steven.burman@gmail.com>
2021-11-27 11:32:42 +1000

    V1

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,21 +13,26 @@ for f in onlyfiles:
     name = f[0:-4]
     file_list[name] = f"{root_dir}/{f}"
 
-print(file_list)
-
 file_choice = st.sidebar.selectbox(
-    "How would you like to be contacted?",
+    "Choose supported project:",
     file_list.keys()
 )
 
 st.title(file_choice)
 
 file_to_load = file_list[file_choice]
-
-st.header(f"Loading: {file_to_load}")
-
 df = pd.read_csv(file_to_load)[["category", "trait", "count", "rarity_score"]]
 
-top = df.sort_values(['rarity_score'], ascending=False).head(30)
-st.dataframe(data=top, height=1000)
+cat_list = ["All"] + list(df["category"].unique())
 
+cat_selected = st.sidebar.selectbox(
+    "Select a single category:",
+    cat_list
+)
+
+if cat_selected is None or cat_selected == "All":
+    _ = df.sort_values(['rarity_score'], ascending=False)
+    st.dataframe(data=_, height=800)
+else:
+    _ = df[df["category"] == cat_selected].sort_values(['rarity_score'], ascending=False)
+    st.dataframe(data=_, height=800)
