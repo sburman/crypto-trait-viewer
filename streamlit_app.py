2582a1b57e8e353e02ec426fd04242298c4d6eae
Steven Burman <steven.burman@gmail.com>
2021-11-27 11:24:24 +1000

    First take

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,93 +1,33 @@
 import streamlit as st
-from google.oauth2 import service_account
-from googleapiclient.discovery import build
 import pandas as pd
+import numpy as np
 
-SCOPE = "https://www.googleapis.com/auth/spreadsheets"
-SPREADSHEET_ID = "1QlPTiVvfRM82snGN6LELpNkOwVI1_Mp9J9xeJe-QoaA"
-SHEET_NAME = "Database"
-GSHEET_URL = f"https://docs.google.com/spreadsheets/d/{SPREADSHEET_ID}"
+from os import listdir
+from os.path import isfile, join
 
+root_dir = './data'
+onlyfiles = [f for f in listdir(root_dir) if isfile(join(root_dir, f))]
 
-@st.experimental_singleton()
-def connect_to_gsheet():
-    # Create a connection object.
-    credentials = service_account.Credentials.from_service_account_info(
-        st.secrets["gcp_service_account"],
-        scopes=[SCOPE],
-    )
+file_list = {}
+for f in onlyfiles:
+    name = f[0:-4]
+    file_list[name] = f"{root_dir}/{f}"
 
-    service = build("sheets", "v4", credentials=credentials)
-    gsheet_connector = service.spreadsheets()
-    return gsheet_connector
+print(file_list)
 
-
-def get_data(gsheet_connector) -> pd.DataFrame:
-    values = (
-        gsheet_connector.values()
-        .get(
-            spreadsheetId=SPREADSHEET_ID,
-            range=f"{SHEET_NAME}!A:E",
-        )
-        .execute()
-    )
-
-    df = pd.DataFrame(values["values"])
-    df.columns = df.iloc[0]
-    df = df[1:]
-    return df
-
-
-def add_row_to_gsheet(gsheet_connector, row) -> None:
-    values = (
-        gsheet_connector.values()
-        .append(
-            spreadsheetId=SPREADSHEET_ID,
-            range=f"{SHEET_NAME}!A:E",
-            body=dict(values=row),
-            valueInputOption="USER_ENTERED",
-        )
-        .execute()
-    )
-
-
-st.set_page_config(page_title="Bug report", page_icon="🐞", layout="centered")
-
-st.title("🐞 Bug report!")
-
-gsheet_connector = connect_to_gsheet()
-
-st.sidebar.write(
-    f"This app shows how a Streamlit app can interact easily with a [Google Sheet]({GSHEET_URL}) to read or store data."
+file_choice = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    file_list.keys()
 )
 
-st.sidebar.write(
-    f"[Read more](https://docs.streamlit.io/knowledge-base/tutorials/databases/public-gsheet) about connecting your Streamlit app to Google Sheets."
-)
+st.title(file_choice)
 
-form = st.form(key="annotation")
+file_to_load = file_list[file_choice]
 
-with form:
-    cols = st.columns((1, 1))
-    author = cols[0].text_input("Report author:")
-    bug_type = cols[1].selectbox(
-        "Bug type:", ["Front-end", "Back-end", "Data related", "404"], index=2
-    )
-    comment = st.text_area("Comment:")
-    cols = st.columns(2)
-    date = cols[0].date_input("Bug date occurrence:")
-    bug_severity = cols[1].slider("Bug severity:", 1, 5, 2)
-    submitted = st.form_submit_button(label="Submit")
+st.header(f"Loading: {file_to_load}")
 
+df = pd.read_csv(file_to_load)[["category", "trait", "count", "rarity_score"]]
 
-if submitted:
-    add_row_to_gsheet(
-        gsheet_connector, [[author, bug_type, comment, str(date), bug_severity]]
-    )
-    st.success("Thanks! Your bug was recorded.")
-    st.balloons()
+top = df.sort_values(['rarity_score'], ascending=False).head(30)
+st.dataframe(data=top, height=1000)
 
-expander = st.expander("See all records")
-with expander:
-    st.write(f"Open original [Google Sheet]({GSHEET_URL})")
-    st.dataframe(get_data(gsheet_connector))
