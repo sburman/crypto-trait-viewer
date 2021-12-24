5b0216815aa1617489790665d00251b2d2f2a7d4
Steven Burman <steven.burman@gmail.com>
2021-12-24 16:38:17 +1000

    Add some analysis charting

--- ballerz/sales_history.py
+++ ballerz/sales_history.py
@@ -17,7 +17,7 @@ def display() -> Any:
     df = pd.read_csv(HISTORY_FILE, parse_dates=['datetime'])
     df = pd.merge(df, BALLERZ, on="baller_id", how="left")
 
-    # st.write(list(df.columns))
+    selected_view = st.sidebar.selectbox("Select view", ["Timeline", "Trait Analysis"])
 
     # some display touch ups to the df
     df['time_axis'] = df['timestamp'].apply(lambda x: arrow.get(x).datetime)
@@ -39,34 +39,77 @@ def display() -> Any:
     col2.metric(f"Last 24hr sales", hours_24_sales)
     col3.metric(f"Previous 24hr sales", hours_48_sales - hours_24_sales)
     
-    days_history = st.sidebar.select_slider("Show number of days", options=[1, 2, 3, 4, 5, 6, 7], value=1)
-    max_price = st.sidebar.slider("Filter max price", 0, int(df["price"].max()), 5000)
+    days_history = st.sidebar.select_slider("Show number of days", options=[1, 2, 3, 4, 5, 6, 7], value=3)
     show_from = most_recent.shift(days=(0 - days_history))
-
     df = df[df['time_axis'] > show_from.datetime]
 
-    selected_category = st.sidebar.selectbox(
-        "Color highlighter:",
-        ["Role", "Body", "Team", "Gender"]
-    )
+    max_price = st.sidebar.slider("Filter max price", 0, int(df["price"].max()), 5000)
 
-    c = alt.Chart(df).mark_circle().encode(
-        x=alt.X('time_axis',
-            scale=alt.Scale(zero=False),
-            axis=alt.Axis(title="Time", format='%b %d %H:%M')
-        ),
-        y=alt.X(
-            'price',
-            scale=alt.Scale(domain=(5, max_price), clamp=True),
-            axis=alt.Axis(title="Price ($USD)")),
-        size=alt.Size('combo_size', legend=None),
-        color=alt.Color(selected_category, legend=None),
-        href='link',
-        tooltip=['baller_id', 'price', 'ago', 'combo', 'rarity', 'skill']
-    ).properties(
-        height=1000
-    )
+    if selected_view == "Timeline":
 
-    line = c.transform_loess('time_axis', 'price').mark_line(size=8, stroke='#ed0c0c')
+        selected_category = st.sidebar.selectbox(
+            "Color highlighter:",
+            ["Role", "Body", "Team", "Gender"]
+        )
 
-    st.altair_chart(c + line, use_container_width=True)
+        c = alt.Chart(df).mark_circle().encode(
+            x=alt.X('time_axis',
+                scale=alt.Scale(zero=False),
+                axis=alt.Axis(title="Time", format='%b %d %H:%M')
+            ),
+            y=alt.X(
+                'price',
+                scale=alt.Scale(domain=(5, max_price), clamp=True),
+                axis=alt.Axis(title="Price ($USD)")),
+            size=alt.Size('combo_size', legend=None),
+            color=alt.Color(selected_category),
+            href='link',
+            tooltip=['baller_id', 'price', 'ago', 'combo', 'rarity', 'skill']
+        ).properties(
+            height=1000
+        )
+
+        line = c.transform_loess('time_axis', 'price').mark_line(size=10, stroke='#ed0c0c')
+
+        st.altair_chart(c + line, use_container_width=True)
+
+    elif selected_view == "Trait Analysis":
+
+        selected_category = st.sidebar.selectbox(
+            "Analysis category:",
+            ["Role", "Body", "Team", "Gender"]
+        )
+
+        stripplot = alt.Chart(df, width=50).mark_circle(size=20).encode(
+            x=alt.X(
+                'jitter:Q',
+                title=None,
+                axis=alt.Axis(values=[0], ticks=True, grid=False, labels=False),
+                scale=alt.Scale(),
+            ),
+            y=alt.Y('price', scale=alt.Scale(domain=(5, max_price), clamp=True)),
+            color=alt.Color(selected_category, legend=None),
+            column=alt.Column(
+                selected_category,
+                header=alt.Header(
+                    labelAngle=-90,
+                    title=None,
+                    labelAnchor="middle",
+                    # titleOrient='top',
+                    # labelOrient='top',
+                    labelAlign='left',
+                    labelPadding=3,
+                ),
+            ),
+        ).transform_calculate(
+            # Generate Gaussian jitter with a Box-Muller transform
+            jitter='sqrt(-2*log(random()))*cos(2*PI*random())'
+        ).configure_facet(
+            spacing=0
+        ).configure_view(
+            stroke=None
+        ).properties(
+            height=800
+        )
+
+        st.altair_chart(stripplot)
