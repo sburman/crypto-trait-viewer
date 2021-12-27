b1d2efe261b888c91ba13b4ab89f9c9383a07caf
Steven Burman <steven.burman@gmail.com>
2021-12-28 09:36:29 +1000

    Chart updates

--- ballerz/sales_history.py
+++ ballerz/sales_history.py
@@ -1,9 +1,11 @@
 import streamlit as st
 
 import altair as alt
+import altair_catplot as altcat
 import arrow
 import datetime
 import json
+import numpy as np
 import pandas as pd
 import requests
 
@@ -27,7 +29,7 @@ def display() -> Any:
     df.index = df.index.astype('datetime64[ns]')
     df = df.join(BALLERZ, on="baller_id", how="left", rsuffix="b_")
 
-    selected_view = st.sidebar.selectbox("Select view", ["Timeline", "Trait Analysis"])
+    selected_view = st.sidebar.selectbox("Select view", ["Timeline", "Time Analysis", "Trait Analysis"], index=1)
 
     # some display touch ups to the df
     df['combo_size'] = 10_000 - df['combo']
@@ -44,20 +46,22 @@ def display() -> Any:
 
     currency_format = "${:0.0f}"
 
-    col1, col2, col3 = st.columns(3)
-    col1.metric(f"Overall Sale Count (updated {arrow.get(most_recent).humanize()})", df.shape[0])
-    col2.metric(f"Average Sale", currency_format.format(df['price'].mean()))
-    col3.metric(f"Total Sales", human_format(df['price'].sum()))
-    
-    col1, col2, col3 = st.columns(3)
-    col1.metric(f"24hr Sale Count", hours_24_sales.shape[0])
-    col2.metric(f"24hr Median Sale", currency_format.format(hours_24_sales['price'].median()))
-    col3.metric(f"24hr Total Sales", human_format(hours_24_sales['price'].sum()))
+    show_metrics = st.checkbox("Show sales summary metrics")
+    if show_metrics:
+        col1, col2, col3 = st.columns(3)
+        col1.metric(f"Overall Sale Count (updated {arrow.get(most_recent).humanize()})", df.shape[0])
+        col2.metric(f"Average Sale", currency_format.format(df['price'].mean()))
+        col3.metric(f"Total Sales", human_format(df['price'].sum()))
+        
+        col1, col2, col3 = st.columns(3)
+        col1.metric(f"24hr Sale Count", hours_24_sales.shape[0])
+        col2.metric(f"24hr Median Sale", currency_format.format(hours_24_sales['price'].median()))
+        col3.metric(f"24hr Total Sales", human_format(hours_24_sales['price'].sum()))
 
-    col1, col2, col3 = st.columns(3)
-    col1.metric(f"Previous 24hr Sale Count", hours_48_sales.shape[0])
-    col2.metric(f"Previous 24hr Median Sale", currency_format.format(hours_48_sales['price'].median()))
-    col3.metric(f"Previous 24hr Total Sales", human_format(hours_48_sales['price'].sum()))
+        col1, col2, col3 = st.columns(3)
+        col1.metric(f"Previous 24hr Sale Count", hours_48_sales.shape[0])
+        col2.metric(f"Previous 24hr Median Sale", currency_format.format(hours_48_sales['price'].median()))
+        col3.metric(f"Previous 24hr Total Sales", human_format(hours_48_sales['price'].sum()))
 
     
     days_history = st.sidebar.select_slider("Show number of days", options=[1, 2, 3, 4, 5, 6, 7], value=3)
@@ -80,9 +84,10 @@ def display() -> Any:
                 scale=alt.Scale(zero=False),
                 axis=alt.Axis(title="Time", format='%b %d %H:%M')
             ),
-            y=alt.X(
+            y=alt.Y(
                 'price',
-                scale=alt.Scale(domain=(5, max_price), clamp=True),
+                'price',
+                scale=alt.Scale(domain=(0, max_price), clamp=True),
                 axis=alt.Axis(title="Price ($USD)")),
             size=alt.Size('combo_size', legend=None),
             # color=alt.Color(selected_category, legend=None),
@@ -95,6 +100,36 @@ def display() -> Any:
         # line = c.transform_loess('time_axis', 'price').mark_line()
 
         st.altair_chart(c, use_container_width=True)
+
+    elif selected_view == "Time Analysis":
+        
+        analysis_selections = ["combo", "rarity", "skill", "Dunks", "Shooting", "Playmaking", "Defense"]
+        
+        analysis_selection = st.sidebar.selectbox("Analysis category:", analysis_selections)
+        analysis_ind = analysis_selections.index(analysis_selection)
+
+        bins = np.array(range(0, 10001, 500))
+        
+        if analysis_ind > 2:
+            bins = np.array(range(60, 101, 5))
+        
+        df['binned'] = pd.cut(df[analysis_selection], bins=bins, labels=[f"{x}" for x in bins[:-1]])
+        # st.write(df['binned'].value_counts())
+
+        chart = altcat.catplot(df,
+               height=1000,
+               width=800,
+               mark='point',
+               box_mark=dict(strokeWidth=1, opacity=0.8),
+               whisker_mark=dict(strokeWidth=2, opacity=0.8),
+               encoding=dict(
+                            x=alt.X('binned:N', title=analysis_selection), 
+                            y=alt.Y('price:Q', scale=alt.Scale(domain=(0, max_price), clamp=True)),
+                            ),
+               transform='box')
+
+        st.altair_chart(chart, use_container_width=True)
+
 
     elif selected_view == "Trait Analysis":
 
@@ -110,7 +145,7 @@ def display() -> Any:
                 axis=alt.Axis(values=[0], ticks=True, grid=False, labels=False),
                 scale=alt.Scale(),
             ),
-            y=alt.Y('price', scale=alt.Scale(domain=(5, max_price), clamp=True)),
+            y=alt.Y('price', scale=alt.Scale(domain=(0, max_price), clamp=True)),
             color=alt.Color(selected_category, legend=None),
             column=alt.Column(
                 selected_category,
