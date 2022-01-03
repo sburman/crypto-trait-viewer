f13f8b22af99b239e7267b1ba6a1aea996584b97
Steven Burman <steven.burman@gmail.com>
2022-01-03 12:22:59 +1000

    Comment out listings data update

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
-poetry run python -m ballerz.update_listings && \
-git add ballerz/ballerz_listing_history.csv && \
+# poetry run python -m ballerz.update_listings && \
+# git add ballerz/ballerz_listing_history.csv && \
 poetry run python -m ballerz.update_sales && \
 git add ballerz/ballerz_sales_history.csv && \
 git commit -m "Update history" && \
